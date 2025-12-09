26ef49c620a094fe0d89080e88a63400aa9e0179
Tona <tona.chheun@akilabs.co>
2025-12-09 11:26:05 +0700

    add

---
+++ src/fundamentals/objects/main.ts
@@ -0,0 +1,2 @@
+console.log(Object.values({a:1, b:2}))     // [1, 2]
+console.log(Object.entries({a:1, b:2}))    // [["a",1], ["b",2]]
---
+++ src/fundamentals/strings/main.ts
@@ -0,0 +1,2 @@
+console.log("5".padStart(3, "0")) // "005"
+console.log("5".padEnd(3, ".")) // "5.."
