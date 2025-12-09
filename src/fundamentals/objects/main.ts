a5899f142a506e51c27e548018a60943e43f32e4
Tona <tona.chheun@akilabs.co>
2025-12-09 13:25:29 +0700

    add

--- src/fundamentals/objects/main.ts
+++ src/fundamentals/objects/main.ts
@@ -1,2 +1,23 @@
 console.log(Object.values({a:1, b:2}))     // [1, 2]
 console.log(Object.entries({a:1, b:2}))    // [["a",1], ["b",2]]
+
+const obj = {
+    get name() { return "Tona"; }
+};
+console.log(Object.getOwnPropertyDescriptors(obj));
+console.log(obj.name);
+
+const obj2 = {
+    _name: "Tona", // private-like field
+
+    get name() {
+        return this._name;
+    },
+
+    set name(value: string) {
+        this._name = value;
+    }
+}
+
+obj2.name = "Admin";
+console.log(obj2.name);
